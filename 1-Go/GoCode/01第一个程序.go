3e7b5992817f7b33f28f30ed23ae25db0415e306
liuawen <157514367@qq.com>
2021-02-06 23:01:46 +0800

    update

---
+++ 1-Go/GoCode/01第一个程序.go
@@ -0,0 +1,28 @@
+//导入主函数的包  目录 文件夹
+package main
+//fmt format 包含格式化输入输出  一些列功能函数的集合
+//系统会导入所需的包 fmt format 标准输入输出格式包
+import "fmt"
+
+
+//正斜杠\\反斜杆
+//注释  注释不参与程序编译 可以帮助理解程序
+//行注释 只能注释一行
+//快速注释的快捷键 ctrl+/   ctrl + shift + / 块注释
+/*
+块注释
+可以注释多行
+*/
+
+//main 叫主函数 是程序的主入口 程序有且只有一个主函数 main 不是mian
+func main()  {
+	fmt.Println("Hello Go...")
+	//fmt.Println("hello world")
+}
+
+/*func main01() {
+	//在屏幕打印hello world
+	fmt.Println("hello world")
+	fmt.Println("Hello Go...")
+}
+*/
